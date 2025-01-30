49e9ff189a08ce9a45556497686c6c329e0b0cba
LThomp <73206646+LThomp101@users.noreply.github.com>
2025-01-29 19:40:30 -0500

    Update and rename Thompson_ex1.py to FishDischarge.py

--- FishDischarge.py
+++ FishDischarge.py
@@ -1,5 +1,4 @@
 # Laniya Thompson
-# Exercise 1
 
 discharges = open("white_clay_discharge_2017.txt", "r").readlines()
 n = len(discharges)
@@ -8,8 +7,8 @@ qtot = 0.0 #totaling or holder for yearly discharge
 qfish = 100.0 #holder for minumum we can take for the fish needs to be above
 qpump = 5.0 #Max amount you can take per day/ 5 cft/sec
 qdays = 0
-for k in range (n): #Run code on every line "for loop"
-    q = float(discharges[k].split()[3]) # Numbers are column numbers 
+for k in range (n): 
+    q = float(discharges[k].split()[3]) 
     q = max (0.0, q - qfish) #Grabs higher number/ Lower
     q = min (q, qpump) #If over 5 you can only take 5
     qdays = qdays + 1 #Add a day
